078198cfb500abe897473cb4d7abd5850783c170
Alireza Balaei <alirezabalayi7@gmail.com>
2023-02-06 20:17:24 +0330

    testing svg d3.{select, append, attr}

--- directories/svg/js/main.js
+++ directories/svg/js/main.js
@@ -1,1 +1,10 @@
 console.log("main.js loaded...")
+
+let rect = d3
+  .select("#canvas")
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", 20)
+  .attr("height", 20)
+  .attr("fill", "red")
