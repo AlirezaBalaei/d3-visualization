7ef5eabe87c4732cdb35634b2304e9dafe083cd7
Alireza Balaei <alirezabalayi7@gmail.com>
2023-02-07 00:36:59 +0330

    first visualization of tall towers d3.json

--- directories/svg/js/main.js
+++ directories/svg/js/main.js
@@ -1,10 +1,31 @@
-console.log("main.js loaded...")
-
-let rect = d3
-  .select("#canvas")
-  .append("rect")
-  .attr("x", 0)
-  .attr("y", 0)
-  .attr("width", 20)
-  .attr("height", 20)
-  .attr("fill", "red")
+d3.json("/directories/svg/data/buildings.json")
+  .then((data) => {
+    data.forEach((element) => {
+      element.height = parseFloat(element.height)
+    })
+    let svg = d3
+      .select("#chart-area")
+      .append("svg")
+      .attr("class", "shadow border")
+      .attr("width", 1024)
+      .attr("height", 400)
+    let rectangles = svg.selectAll("rect").data(data)
+    rectangles
+      .enter()
+      .append("rect")
+      .attr("x", (data, index) => {
+        return index * 50 + 10
+      })
+      .attr("y", 0)
+      .attr("width", 30)
+      .attr("height", (data, index) => {
+        return data.height
+      })
+      .attr("name", (data) => {
+        return data.name
+      })
+      .attr("fill", "grey")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
