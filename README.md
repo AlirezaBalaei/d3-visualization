f8039de9b9fc045c806f908597442dded1046e97
Alireza Balaei <alirezabalayi7@gmail.com>
2023-02-07 11:15:28 +0330

    using scaleOrdinal and categorial colors

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
+![d3jspic_001](https://user-images.githubusercontent.com/99330644/217182433-502bbb46-18e5-45e7-95e2-9ceaaecf7207.png)
+
 open git bash and run:
-`git clone https://github.com/AlirezaBalaei/d3-visualization.git`
+``git clone https://github.com/AlirezaBalaei/d3-visualization.git``
 
 then open directory:
-`cd d3-visualization`
+``cd d3-visualization``
 
 then install modules:
-`npm install`
+``npm install``
 
 then run this shell command:
-`./node_modules/.bin/http-server -a localhost -p 8000 -c-1`
+``./node_modules/.bin/http-server -a localhost -p 8000 -c-1``
--- directories/svg/js/main.js
+++ directories/svg/js/main.js
@@ -4,7 +4,10 @@ d3.json("/directories/svg/data/buildings.json")
       element.height = parseFloat(element.height)
     })
     // create a scaling function named y
-    let y = d3.scaleLinear().domain([0, 800]).range([0, 400])
+    let y = d3.scaleLinear().domain([0, 800]).range([0, 380])
+    let x = d3.scaleLinear().domain([0, 4]).range([0, 920])
+    let color = d3.scaleOrdinal().domain([0, 4]).range(d3.schemeSet3)
+    console.log(color)
     let svg = d3
       .select("#chart-area")
       .append("svg")
@@ -16,17 +19,20 @@ d3.json("/directories/svg/data/buildings.json")
       .enter()
       .append("rect")
       .attr("x", (data, index) => {
-        return index * 50 + 10
+        console.log(index)
+        return x(index) + 25
       })
       .attr("y", 0)
-      .attr("width", 30)
+      .attr("width", 50)
       .attr("height", (data, index) => {
         return y(data.height)
       })
       .attr("name", (data) => {
         return data.name
       })
-      .attr("fill", "grey")
+      .attr("fill", (data, index) => {
+        return color(index)
+      })
   })
   .catch((err) => {
     console.log(err)
