7d5911411635d65d06c7c8cfaea990ff66a8c7b0
Alireza Balaei <alirezabalayi7@gmail.com>
2023-02-07 20:59:18 +0330

    using band scale to scale bands[F

--- directories/svg/js/main.js
+++ directories/svg/js/main.js
@@ -3,27 +3,33 @@ d3.json("/directories/svg/data/buildings.json")
     data.forEach((element) => {
       element.height = parseFloat(element.height)
     })
-    // create a scaling function named y
-    let y = d3.scaleLinear().domain([0, 800]).range([0, 380])
-    let x = d3.scaleLinear().domain([0, 4]).range([0, 920])
+
+    let svgWidth = 1024
+    let svgHeight = 500
     let color = d3.scaleOrdinal().domain([0, 4]).range(d3.schemeSet3)
-    console.log(color)
+    let y = d3.scaleLinear().domain([0, 800]).range([0, 380])
+    let x = d3
+      .scaleBand()
+      .domain(Object.keys(data))
+      .range([0, svgWidth])
+      .paddingInner(0.3)
+      .paddingOuter(0.2)
+
     let svg = d3
       .select("#chart-area")
       .append("svg")
       .attr("class", "shadow border")
-      .attr("width", 1024)
-      .attr("height", 400)
+      .attr("width", svgWidth)
+      .attr("height", svgHeight)
+
     let rectangles = svg.selectAll("rect").data(data)
+
     rectangles
       .enter()
       .append("rect")
-      .attr("x", (data, index) => {
-        console.log(index)
-        return x(index) + 25
-      })
+      .attr("x", (data, index) => x(index))
       .attr("y", 0)
-      .attr("width", 50)
+      .attr("width", x.bandwidth())
       .attr("height", (data, index) => {
         return y(data.height)
       })
