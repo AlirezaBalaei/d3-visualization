83a861f3ab35c7e3db1112f25d2bf93783e6bd54
Alireza Balaei <alirezabalayi7@gmail.com>
2023-02-09 16:56:54 +0330

    add margin to svg and cleaning code

--- directories/svg/js/main.js
+++ directories/svg/js/main.js
@@ -4,17 +4,29 @@ d3.json("/directories/svg/data/buildings.json")
       element.height = parseFloat(element.height)
     })
 
+    // Essentials
+    let margin = { left: 100, right: 10, top: 10, bottom: 100 }
     let svgWidth = 1024
     let svgHeight = 500
+    let marginedWidth = svgWidth - margin.left - margin.right
+    let marginedHeight = svgHeight - margin.top - margin.bottom
     let color = d3.scaleOrdinal().domain([0, 4]).range(d3.schemeSet3)
-    let y = d3.scaleLinear().domain([0, 800]).range([0, 380])
+
+    // Y attribute
+    let y = d3
+      .scaleLinear()
+      .domain([0, d3.max(data, (d) => d.height)])
+      .range([0, marginedHeight])
+
+    // X attribute
     let x = d3
       .scaleBand()
-      .domain(Object.keys(data))
-      .range([0, svgWidth])
+      .domain(data.map((e) => e.name))
+      .range([0, marginedWidth])
       .paddingInner(0.3)
       .paddingOuter(0.3)
 
+    // SVG
     let svg = d3
       .select("#chart-area")
       .append("svg")
@@ -22,12 +34,17 @@ d3.json("/directories/svg/data/buildings.json")
       .attr("width", svgWidth)
       .attr("height", svgHeight)
 
-    let rectangles = svg.selectAll("rect").data(data)
+    // Group to apply margin inside of svg
+    let g = svg
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`)
 
+    // Bands as rectangle
+    let rectangles = g.selectAll("rect").data(data)
     rectangles
       .enter()
       .append("rect")
-      .attr("x", (data, index) => x(index))
+      .attr("x", (data) => x(data.name))
       .attr("y", 0)
       .attr("width", x.bandwidth())
       .attr("height", (data, index) => {
