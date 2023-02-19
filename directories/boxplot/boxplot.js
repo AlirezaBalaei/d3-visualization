cfbec5575895c97c0b16cc71ae108428a43eb85b
AlirezaBalaei <alirezabalayi7@gmail.com>
2023-02-19 16:11:07 +0330

    added outliers and beeing scalable

--- directories/boxplot/boxplot.js
+++ directories/boxplot/boxplot.js
@@ -1,5 +1,7 @@
 // set the dimensions and margins of the graph
-let strokeColor = "#6497b1"
+let colors={
+  stroke: "#6497b1"
+}
 let margin = {top: 100, right: 80, bottom: 80, left: 80},
     width = window.innerWidth - margin.left - margin.right,
     height = window.innerHeight - margin.top - margin.bottom;
@@ -13,54 +15,45 @@ let svg = d3.select("#chart-wrapper")
           `translate(${margin.left} ,${margin.top})`);
 
 // Read the data and compute summary statistics for each specie
-const dataApiUrl = "https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv"
 d3.csv("data.csv", (data) => {
-
   // Compute quartiles, median, inter quantile range min and max --> these info are then used to draw the box.
   let sumstat = d3
     .nest() // nest function allows to group the calculation per level of a factor
-    .key(d=>d.Species)
+    .key((d) => d.name)
     .rollup((d) => {
-      q1 = d3.quantile(
-        d
-          .map(g=>g.Sepal_Length)
-          .sort(d3.ascending),
-        0.25
-      )
-      median = d3.quantile(
-        d
-          .map(g => g.Sepal_Length)
-          .sort(d3.ascending),
-        0.5
-      )
-      q3 = d3.quantile(
-        d
-          .map(g => g.Sepal_Length)
-          .sort(d3.ascending),
-        0.75
-      )
+      q1 = d3.quantile(d.map((g) => g.value).sort(d3.ascending), 0.25)
+      median = d3.quantile(d.map((g) => g.value).sort(d3.ascending), 0.5)
+      q3 = d3.quantile(d.map((g) => g.value).sort(d3.ascending), 0.75)
       interQuantileRange = q3 - q1
       min = q1 - 1.5 * interQuantileRange
       max = q3 + 1.5 * interQuantileRange
+      outliers = d.map((d) => parseFloat(d.value))
+      outliers = outliers.filter((i) => i < min || i > max)
+      //outliersLength = outliers.length
+      //outliers.push({key: d[0].name}, {length: outliersLength})
+      //console.log("outliers", outliers)
+      //filter(i => Y[i] < r0 || Y[i] > r1)
       return {
         q1: q1,
         median: median,
         q3: q3,
         interQuantileRange: interQuantileRange,
         min: min,
-        max: max
+        max: max,
+        outliers: outliers
       }
     })
     .entries(data)
-
-    console.log(sumstat)
-       
+  const minValue = d3.min(data, (d) => d.value)
+  const maxValue = d3.max(data, (d) => d.value)
+  let dataKeys = sumstat.map((d) => d.key)
   // Show the X scale
-  let x = d3.scaleBand()
-    .range([ 0, width ])
-    .domain(["setosa", "versicolor", "virginica"])
+  let x = d3
+    .scaleBand()
+    .range([0, width])
+    .domain(dataKeys)
     .paddingInner(1)
-    .paddingOuter(.5)
+    .paddingOuter(0.5)
   svg
     .append("g")
     .attr("transform", `translate(0,${height})`)
@@ -69,8 +62,9 @@ d3.csv("data.csv", (data) => {
     .attr("transform", `translate(0,+10)`)
 
   // Show the Y scale
-  let y = d3.scaleLinear()
-    .domain([3,9])
+  let y = d3
+    .scaleLinear()
+    .domain([minValue, maxValue])
     .range([height, 0])
   svg
     .append("g")
@@ -87,44 +81,68 @@ d3.csv("data.csv", (data) => {
     .append("line")
     .attr("x1", (d) => x(d.key))
     .attr("x2", (d) => x(d.key))
-    .attr("y1", (d) => y(d.value.min))
-    .attr("y2", (d) => y(d.value.max))
-    .attr("stroke", strokeColor)
+    .attr("y1", (d) => {
+      return y(d.value.min)
+    })
+    .attr("y2", (d) => {
+      return y(d.value.max)
+    })
+    .attr("stroke", colors.stroke)
     .attr("stroke-width", 6)
     .style("width", 40)
 
-      // rectangle for the main box
-      let boxWidth = 100
-      svg
-        .selectAll("boxes")
-        .data(sumstat)
-        .enter()
-        .append("rect")
-        .attr("x", function (d) {
-          return x(d.key) - boxWidth / 2
-        })
-        .attr("y", function (d) {
-          return y(d.value.q3)
-        })
-        .attr("height", function (d) {
-          return y(d.value.q1) - y(d.value.q3)
-        })
-        .attr("width", boxWidth)
-        .attr("stroke", strokeColor)
-        .attr("stroke-width", 5)
-        .style("fill", "#b3cde0")
-      
-      // Show the median
-      svg
-        .selectAll("medianLines")
-        .data(sumstat)
-        .enter()
-        .append("line")
-        .attr("x1", (d) => x(d.key) - boxWidth / 2)
-        .attr("x2", (d) => x(d.key) + boxWidth / 2)
-        .attr("y1", (d) => y(d.value.median))
-        .attr("y2", (d) => y(d.value.median))
-        .attr("stroke", strokeColor)
-        .attr("stroke-width", 5)
-        .style("width", 80)
+  // rectangle for the main box
+  let boxWidth = 100
+  svg
+    .selectAll("boxes")
+    .data(sumstat)
+    .enter()
+    .append("rect")
+    .attr("x", function (d) {
+      return x(d.key) - boxWidth / 2
+    })
+    .attr("y", function (d) {
+      return y(d.value.q3)
+    })
+    .attr("height", function (d) {
+      return y(d.value.q1) - y(d.value.q3)
+    })
+    .attr("width", boxWidth)
+    .attr("stroke", colors.stroke)
+    .attr("stroke-width", 5)
+    .style("fill", "#b3cde0")
+
+  // Show the median
+  svg
+    .selectAll("medianLines")
+    .data(sumstat)
+    .enter()
+    .append("line")
+    .attr("x1", (d) => x(d.key) - boxWidth / 2)
+    .attr("x2", (d) => x(d.key) + boxWidth / 2)
+    .attr("y1", (d) => y(d.value.median))
+    .attr("y2", (d) => y(d.value.median))
+    .attr("stroke", colors.stroke)
+    .attr("stroke-width", 5)
+    .style("width", 80)
+
+  // Show the outlier circles    
+  let outliersData = []
+    console.log(sumstat)
+    sumstat.forEach((candel)=>{
+      for (let i = 0; i < candel.value.outliers.length; i++) {
+        outliersData.push({key: candel.key, value: candel.value.outliers[i]})
+        
+      }
+    })
+  let circles = svg
+    .selectAll("circle")
+    .data(outliersData)
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => x(d.key))
+    .attr("cy", (d) => y(d.value))
+    .attr("r", 3)
+    .style("opacity", 0.8)
+    .style("fill", colors.stroke)
 })
