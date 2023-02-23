a382652e63e628e8ad9ae7ba15369257c89560c3
alirezabalayi <alirezabalayi@pro.ai>
2023-02-23 11:08:22 +0330

    scale time instead of data index

--- directories/areachart/areachart.js
+++ directories/areachart/areachart.js
@@ -10,6 +10,17 @@ let months = [
   "January", "February", "March", "April", "May", "June", "July", "August",
   "September", "October", "November", "December"
 ]
+// give "January" to this function and it will return `new Date()` of that
+let parseMonths = d3.timeParse("%B")
+console.log(d3.extent(months , m=>parseMonths(m)))
+//Millisecond ==> .%L
+//Second ==> :%S
+//Minute ==> %I:%M
+//Hour ==> %I %p
+//Day ==> %a %d
+//Week ==> %b %d
+//Month ==> %B
+//Year ==> %Y
 
 data = data.sort((a,b)=>a-b)
 console.log(data)
@@ -23,9 +34,28 @@ graph = svg
 .attr("height", height-margin.left-margin.right)
 .attr("transform", `translate(${margin.left},${margin.right})`)
 
-let graphWidth = d3.select(".graph").attr("width")
-let graphHeight = d3.select(".graph").attr("height")
-let x = d3.scaleLinear().domain([0,data.length]).range([0,graphWidth])
-let y = d3.scaleLinear().domain([data[0],data[data.length-1]]).range([graphHeight,0])
-let area = d3.area().x((d,i)=>x(i)).y0(graphHeight).y1(d=>y(d)).curve(d3.curveBasis)
-graph.append("path").attr("fill", color[4]).attr("stroke", color[3]).attr("d", area(data))
+let graphWidth = d3
+  .select(".graph")
+  .attr("width")
+let graphHeight = d3
+  .select(".graph")
+  .attr("height")
+let x = d3
+  .scaleLinear()
+  .domain(d3.extent(months, m=>parseMonths(m)))
+  .range([0,graphWidth])
+let y = d3
+  .scaleLinear()
+  .domain([data[0],data[data.length-1]])
+  .range([graphHeight,0])
+let area = d3
+  .area()
+  .x((d,i)=>x(parseMonths(months[i])))
+  .y0(graphHeight)
+  .y1(d=>y(d))
+  .curve(d3.curveBasis)
+graph
+  .append("path")
+  .attr("fill", color[4])
+  .attr("stroke", color[3])
+  .attr("d", area(data))
